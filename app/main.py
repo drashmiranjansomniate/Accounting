53f4b0d37d013df67fdfdbc190ea51f5db5e09fa
prasant-somniate <prasant@somniatetech.com>
2026-05-12 18:49:24 +0530

    purchase backend

--- app/main.py
+++ app/main.py
@@ -1,4 +1,41 @@
+# from fastapi import FastAPI
+
+# from app.core.database import Base
+# from app.core.database import engine
+
+# from app.modules.vendors.model import Vendor
+# from app.modules.vendors.routes import router as vendor_router
+
+# from app.modules.purchase_orders.model import (
+#     PurchaseOrder,
+#     PurchaseOrderItem
+# )
+
+# from app.modules.purchase_orders.routes import (
+#     router as purchase_order_router
+# )
+
+# Base.metadata.create_all(bind=engine)
+
+# app = FastAPI(
+#     title="Accounting SaaS API",
+#     version="1.0.0"
+# )
+
+# app.include_router(vendor_router)
+# app.include_router(purchase_order_router)
+
+
+# @app.get("/")
+# def home():
+#     return {
+#         "message": "Accounting Backend Running Successfully"
+#     }
+
+
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.database import Base
 from app.core.database import engine
@@ -22,6 +59,19 @@ app = FastAPI(
     version="1.0.0"
 )
 
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Routers
 app.include_router(vendor_router)
 app.include_router(purchase_order_router)
 
--- app/modules/vendors/service.py
+++ app/modules/vendors/service.py
@@ -21,8 +21,8 @@ def create_vendor_service(
 ):
     return create_vendor_repo(
         db=db,
-        vendor=vendor
     )
+        vendor=vendor
 
 
 def get_all_vendors_service(
